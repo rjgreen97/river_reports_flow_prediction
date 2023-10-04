ffdc6bb9bfae247108ec3b9fbafb2182d0163669
RJ Green <rjgreen97@gmail.com>
2023-10-04 16:29:21 -0600

    work on forecasting plot logic

--- flow_forecaster.py
+++ flow_forecaster.py
@@ -1,6 +1,7 @@
 from river_data_fetcher import RiverDataFetcher
 from neuralprophet import NeuralProphet, set_log_level
 import pandas as pd
+import os
 
 set_log_level("ERROR")
 
@@ -8,21 +9,29 @@ set_log_level("ERROR")
 class FlowForecaster:
     def __init__(self, site_id: str):
         self.site_id = site_id
-        self.df = self._get_df()
         self.model = NeuralProphet()
+        self.df = self._get_df()
+        self.source_name = self._get_site_source_name()
 
-    def forecast(self):
+    def forecast(self) -> None:
+        self.train()
         df_future = self.model.make_future_dataframe(self.df, n_historic_predictions=True, periods=365)
         forecast = self.model.predict(df_future)
+        plot = self.model.plot(forecast)
+        plot.write_image(os.path.join("plots", "forecasted_flow", f"{self.source_name}_forecast.png"))
 
-    def train(self):
+    def train(self) -> None:
         self.model.fit(self.df, freq="H")
 
     def _get_df(self) -> pd.DataFrame:
         river_data_fetcher = RiverDataFetcher(self.site_id)
         return river_data_fetcher.generate_df()
 
+    def _get_site_source_name(self) -> str:
+        river_data_fetcher = RiverDataFetcher(self.site_id)
+        return river_data_fetcher.source_name.lower().replace(" ", "_").replace(",", "")
+
 
 if __name__ == "__main__":
-    flow_forecaster = FlowForecaster("2813acc0-1dc3-413f-b27c-284e934732a2")
-    print(flow_forecaster.df.head())
+    flow_forecaster = FlowForecaster("03a427ca-f37e-4232-8144-3d33f2aeb0c5")
+    flow_forecaster.forecast()
--- river_data_fetcher.py
+++ river_data_fetcher.py
@@ -2,27 +2,37 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import os
+import uuid
+
 
 
 class RiverDataFetcher:
     def __init__(self, site_id: str):
         self.site_id = site_id
+        self.session = self._start_session()
+        self.source_name = self.get_site_source_name()
 
     def generate_df(self) -> pd.DataFrame:
-        self.session = self._start_session()
         self.result = self._get_site_flow_data()
         self.river_df = self._get_date_and_flow_data()
         self.session.close()
         return self.river_df
 
     def plot(self) -> None:
-        source_name = self._get_site_source_name()
-        plot_file_source_name = source_name.lower().replace(" ", "_").replace(",", "")
+        plot_file_source_name = self.source_name.lower().replace(" ", "_").replace(",", "")
         plt = self.river_df.plot(x="ds", y="y", figsize=(12, 6), grid=True)
         plt.set_xlabel("Date")
         plt.set_ylabel("Flow (cfs)")
-        plt.set_title(f"{source_name}")
-        plt.figure.savefig(os.path.join("plots", f"{plot_file_source_name}_plot.png"))
+        plt.set_title(f"{self.source_name}")
+        plt.figure.savefig(os.path.join("plots", "historical_flow", f"{plot_file_source_name}_historical.png"))
+        self.session.close()
+
+    def get_site_source_name(self) -> str:
+        raw_sql = text(f"select * from rr.site where id = '{self.site_id}'")
+        site_data = self.session.execute(raw_sql).fetchall()
+        site_data_df = pd.DataFrame(site_data)
+        self.session.close()
+        return site_data_df["source_name"].values[0]
 
     def _start_session(self) -> sessionmaker:
         engine = create_engine("postgresql://rjgreen@localhost/riverreports")
@@ -31,14 +41,15 @@ class RiverDataFetcher:
 
     def _get_site_flow_data(self) -> list:
         raw_sql = text(
-            f"select * from rr.flow where site_id = '{self.site_id}' order by created_at desc"
+            f"select avg(value) as value, cast(ts as date) from rr.flow where site_id = \
+            '{self.site_id}' group by cast(ts as date) order by ts asc"
         )
         return self.session.execute(raw_sql)
 
     def _get_date_and_flow_data(self) -> pd.DataFrame:
         df = pd.DataFrame(self.result.fetchall())
         y = df["value"]
-        ds = df["created_at"]
+        ds = df["ts"]
         river_df = pd.DataFrame({"ds": ds, "y": y})
         river_df["ds"] = pd.to_datetime(river_df["ds"], utc=True)
         river_df["ds"] = river_df["ds"].dt.strftime("%Y-%m-%d %H:%M:%S")
@@ -47,15 +58,16 @@ class RiverDataFetcher:
         river_df = river_df.drop_duplicates(subset='ds', keep='first')
         return river_df.reset_index(drop=True)
 
-    def _get_site_source_name(self) -> str:
-        raw_sql = text(f"select * from rr.site where id = '{self.site_id}'")
-        site_data = self.session.execute(raw_sql).fetchall()
-        site_data_df = pd.DataFrame(site_data)
-        return site_data_df["source_name"].values[0]
+    def get_all_sites(self) -> list:
+        raw_sql = text("select distinct site_id from rr.flow;")
+        site_ids = self.session.execute(raw_sql).fetchall()
+        site_ids_df = pd.DataFrame(site_ids)
+        uuid_list = site_ids_df['site_id'].tolist()
+        self.session.close()
+        return [str(uuid_obj) for uuid_obj in uuid_list]
 
 
 if __name__ == "__main__":
     river_data_fetcher = RiverDataFetcher("2813acc0-1dc3-413f-b27c-284e934732a2")
     df = river_data_fetcher.generate_df()
     river_data_fetcher.plot()
-    print(df)
