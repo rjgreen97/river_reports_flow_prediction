5332fc4226770f86f50115fd0fd47fb6275cff21
RJ Green <rjgreen97@gmail.com>
2023-11-21 06:13:24 -0700

    add variables to bin scripts

--- bin/upload_forecast_prod_db.sh
+++ bin/upload_forecast_prod_db.sh
@@ -10,9 +10,15 @@ LOCAL_DB_USER="rjgreen"
 TABLE_NAME="rr.forecast"
 PATH_TO_LOCAL_FORECAST_TABLE="/Users/rjgreen/forecast.sql"
 
+# Dump forecast table locally
 echo "Downloading forecast table..."
 pg_dump -U LOCAL_DB_USER -d LOCAL_DB_NAME -t TABLE_NAME -f forecast.sql
 
-
+# Move forecast table to production database
 echo "Moving forecast table to production database..."
 psql -U REMOTE_DB_USER -h REMOTE_DB_HOST -p REMOTE_DB_PORT -d REMOTE_DB_NAME -a -f PATH_TO_LOCAL_FORECAST_TABLE
+
+# Clean up forecast file
+rm forecast.sql
+
+echo "Data transfer complete!"
