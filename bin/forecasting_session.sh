a5e7911339e2f995503242b1dc86f83ac0003f14
Ubuntu <ubuntu@ip-172-31-9-66.us-west-2.compute.internal>
2023-12-18 19:27:28 +0000

    run from anywhere

--- bin/forecasting_session.sh
+++ bin/forecasting_session.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# move to the root of the project
+cd "${SCRIPT_DIR}/../"
 
 python3 -m src.forecasting_session
