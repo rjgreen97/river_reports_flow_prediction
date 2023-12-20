bac5fb129fec8c0d0fb942e8040569b8d143299a
RJ Green <rjgreen97@gmail.com>
2023-12-20 15:47:13 -0700

    update bin script for python3.10

--- bin/forecasting_session.sh
+++ bin/forecasting_session.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # move to the root of the project
 cd "${SCRIPT_DIR}/../"
 
-python3 -m src.forecasting_session
+python3.10 -m src.forecasting_session
